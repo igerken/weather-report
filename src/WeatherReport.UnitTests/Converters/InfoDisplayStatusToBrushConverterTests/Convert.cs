dc9ef8d89e4f60fdfaa1285a19eb4e8c6e935ab2
Ivan Gerken <i_gerken@yahoo.com>
2024-11-11 10:56:16 +0100

    Tests for InfoDisplayStatusToBrushConverter

--- src/WeatherReport.UnitTests/Converters/InfoDisplayStatusToBrushConverterTests/Convert.cs
+++ src/WeatherReport.UnitTests/Converters/InfoDisplayStatusToBrushConverterTests/Convert.cs
@@ -12,15 +12,34 @@ public class Convert
     public void Convert_WhenCalledWithInfoDisplayStatusError_ReturnsErrorBrush()
     {
         // Arrange
+        Color errorColor = Color.FromRgb(255, 0, 0);
         var converter = new InfoDisplayStatusToBrushConverter();
-        converter.ErrorColor = Color.FromRgb(255, 0, 0);
+        converter.ErrorColor = errorColor;
         converter.NormalColor = Color.FromRgb(0, 255, 0);
 
         // Act
         var result = converter.Convert(InfoDisplayStatus.Error, typeof(Brush), null, null);
 
         // Assert
-        Assert.IsAssignableFrom<System.Windows.Media.Brush>(result);
-        Assert.Equal(System.Windows.Media.Color.FromRgb(255, 0, 0), ((System.Windows.Media.SolidColorBrush)result).Color);
+        Assert.IsAssignableFrom<Brush>(result);
+        Assert.Equal(errorColor, ((SolidColorBrush)result).Color);
+    }
+
+    // Unit test for Convert method of InfoDisplayStatusToBrushConverter class for InfoDisplayStatus which represents the normal status
+    [Fact]
+    public void Convert_WhenCalledWithInfoDisplayStatusNormal_ReturnsNormalBrush()
+    {
+        // Arrange
+        Color normalColor = Color.FromRgb(0, 255, 0);
+        var converter = new InfoDisplayStatusToBrushConverter();
+        converter.ErrorColor = Color.FromRgb(255, 0, 0);
+        converter.NormalColor = normalColor;
+
+        // Act
+        var result = converter.Convert(InfoDisplayStatus.Normal, typeof(Brush), null, null);
+
+        // Assert
+        Assert.IsAssignableFrom<Brush>(result);
+        Assert.Equal(normalColor, ((SolidColorBrush)result).Color);
     }
 }
